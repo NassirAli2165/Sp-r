728b14ae30f464d2cb9a2f2cf966e874019cf44b
Kay James <mmj342@nyu.edu>
2020-05-01 00:21:03 -0400

    database and sign in half implemented

---
+++ Spår/Spår/NSObject+SignInViewController.h
@@ -0,0 +1,20 @@
+//
+//  NSObject+SignInViewController.h
+//  Spår
+//
+//  Created by user162319 on 4/30/20.
+//  Copyright © 2020 Nassir Ali. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSObject ()
+
+@end
+
+NS_ASSUME_NONNULL_END
